ec02cf98af14032ba7032261f325746a20339765
Gurmanpreet Kaur <gurmanpreet341.be22@chitkara.edu.in>
2025-05-18 11:55:01 +0530

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "sonner";
@@ -16,36 +17,55 @@ import Blog from "./pages/Blog";
 import Payment from './pages/Payment';
 import ProtectedRoute from './components/ProtectedRoute';
 import Profile from "./pages/Profile";
+
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      {/* <Navbar /> Uncomment Navbar if you want it visible on all pages */}
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<Index />} /> {/* Public Home page */}
+const App = () => {
+  const [message, setMessage] = useState("");
 
-        {/* Protected Routes (requires login) */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/restaurants" element={<Restaurants />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/payment" element={<Payment />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/profile" element={<Profile />} />
-        </Route>
+  useEffect(() => {
+    fetch("https://grubzap.onrender.com")  // your backend URL
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message || "Welcome to GrubZap!"))
+      .catch(() => setMessage("Failed to fetch backend message."));
+  }, []);
 
-        {/* Catch-All for 404 */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Toaster />
-    </BrowserRouter>
-  </QueryClientProvider>
-);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        {/* Optionally show Navbar on all pages */}
+        {/* <Navbar /> */}
+
+        {/* Show message somewhere, e.g., top of the app */}
+        <div style={{ textAlign: "center", margin: "1rem", fontWeight: "bold" }}>
+          {message}
+        </div>
+
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Index />} /> {/* Public Home page */}
+
+          {/* Protected Routes (requires login) */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/restaurants" element={<Restaurants />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/payment" element={<Payment />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+
+          {/* Catch-All for 404 */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Toaster />
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
