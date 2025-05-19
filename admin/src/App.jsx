725fa93a52bfe6a278dbcf33a82484d7ea163a68
Gurnimrat <singhmarwaha2004@gmail.com>
2025-05-20 01:15:38 +0530

    done admin page login and signup and beautified admin page and logout feature of admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation, Navigate } from 'react-router-dom'
 import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
 import Add from './pages/Add/Add' 
+import Login from './pages/Login/Login'
+import Signup from './pages/Signup/Signup'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   const url="http://localhost:4000"
+  const location = useLocation();
+  const noNavPaths = ['/login', '/signup'];
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const showNav = !noNavPaths.includes(location.pathname);
+
+  // Handlers to update authentication state, to be passed to Login and Signup
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+  };
+
+  if (!isAuthenticated && !noNavPaths.includes(location.pathname)) {
+    return <Navigate to="/login" replace />;
+  }
+
   return (
     <div>
       <ToastContainer />
-      <Navbar/>
-      <hr/>
+      {showNav && <Navbar onLogout={handleLogout} />}
+      {showNav && <hr />}
       <div className="app-content">
-        <Sidebar/>
+        {showNav && <Sidebar />}
         <Routes>
+          <Route path="/login" element={<Login onLogin={handleLogin} />} />
+          <Route path="/signup" element={<Signup onSignup={handleLogin} />} />
           <Route path="/add" element={<Add url={url}/>} />
           <Route path="/list" element={<List url={url}/>} />
           <Route path="/orders" element={<Orders url={url}/>} />
