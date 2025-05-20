b55e13ed5dfe1f69a78e048d2b773e729501e4c0
Gurnimrat <singhmarwaha2004@gmail.com>
2025-05-20 14:08:21 +0530

    corrected

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -35,7 +35,7 @@ const Add = ({url}) => {
                 name:"",
                 description:"",
                 price:"",
-                category:"Sushi"
+                category:"Pizza"
             })  
             setImage(false);
             toast.success(response.data.message);
